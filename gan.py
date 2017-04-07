fe7608c2677a93726e8c2b634fe1af15b6bafec9
Anudeep Konda <Anudeep@d-172-25-203-230.dhcp.virginia.edu>
2017-04-07 13:39:07 -0400

    Fixed the tensor shape issue

--- gan.py
+++ gan.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-import numpy as np
+import numpy as np, os, sys, re
 import random, json, string, pickle
 import keras
 from keras.models import Sequential
@@ -7,19 +7,56 @@ from keras.models import Model
 import keras.layers
 from keras.layers import Merge, Dense, merge
 import keras.models
-import keras.optimizers
+from keras.optimizers import SGD
 import keras.callbacks
+from keras.preprocessing import image
 
+img_names = []
+encoding = []
+file_path = os.path.dirname(os.path.abspath(__file__)) + "/pubfig_imgs"
+with open("pubfig_attributes.txt") as img_file:
+    count = 0
+    print("Starting to fetch attributes")
+    count = 0
+    for line in img_file:
+        arr = re.split(' |\t', line)
+        if arr[0] == '#':
+            continue
+        img_name = arr[0]
+        num = 0
+        for i in range(1,20):
+            try:
+                test = int(arr[i])
+                img_name += '-'+arr[i]
+                num = i
+                break
+            except:
+                img_name += '-'+arr[i]
+        img_name += ".jpg"
+        img_attr = arr[num:]
+        for i in range(len(img_attr)):
+            item = float(img_attr[i])
+            if item <= 0:
+                img_attr[i] = 0
+            else:
+                img_attr[i] = 1
+        img_names.append(img_name)
+        encoding.append(img_attr)
+        count += 1
+        if count % 10000 == 0:
+            print("10000 more done")
 
+encoding = np.asarray(encoding)
+img_names = np.asarray(img_names)
 
-gen1_input = keras.layers.Input(shape=(4, 4, 1024))
-real_image = keras.layers.Input(shape=(128, 128, 3))
-input_vector = keras.layers.Input(shape=(4, 4, 1024))
 concatenated_vector = keras.layers.Input(shape=(4, 4, 128))
-di2_input = keras.layers.Input(shape=(256, 256, 6))
-di1_input = keras.layers.Input(shape=(128, 128, 6))
-gen2_input = keras.layers.Input(shape=(128, 128, 3))
 
+gen1_input = keras.layers.Input(shape=(4, 4, 1152))
+di1_input = keras.layers.Input(shape=(128, 128, 3))
+real_image = keras.layers.Input(shape=(128, 128, 3))
+
+di2_input = keras.layers.Input(shape=(256, 256, 6))
+gen2_input = keras.layers.Input(shape=(128, 128, 3))
 main_model = Sequential()
 full_model = Sequential()
 
@@ -42,7 +79,7 @@ text_input.add(keras.layers.convolutional.Conv2D(128, 1, strides=(1, 1), padding
 
 ########################################  S 1 Generator ###############################################
 
-s1_gen = keras.layers.convolutional.Conv2DTranspose(512, (4, 4), strides=(2, 2), padding='valid', data_format='channels_last', activation='relu', use_bias=True, kernel_initializer='glorot_uniform', bias_initializer='zeros', kernel_regularizer=None, bias_regularizer=None, activity_regularizer=None, kernel_constraint=None, bias_constraint=None, input_shape=(4, 4, 1024))(gen1_input)
+s1_gen = keras.layers.convolutional.Conv2DTranspose(512, (4, 4), strides=(2, 2), padding='valid', data_format='channels_last', activation='relu', use_bias=True, kernel_initializer='glorot_uniform', bias_initializer='zeros', kernel_regularizer=None, bias_regularizer=None, activity_regularizer=None, kernel_constraint=None, bias_constraint=None, input_shape=(4, 4, 1152))(gen1_input)
 s1_gen = keras.layers.convolutional.Conv2D(512, 3, strides=(1, 1), padding='valid', data_format='channels_last', dilation_rate=(1, 1), activation='relu', use_bias=True, kernel_initializer='glorot_uniform', bias_initializer='zeros', kernel_regularizer=None, bias_regularizer=None, activity_regularizer=None, kernel_constraint=None, bias_constraint=None)(s1_gen)
 
 s1_gen = keras.layers.convolutional.Conv2DTranspose(256, (4, 4), strides=(2, 2), padding='valid', data_format='channels_last', activation='relu', use_bias=True, kernel_initializer='glorot_uniform', bias_initializer='zeros', kernel_regularizer=None, bias_regularizer=None, activity_regularizer=None, kernel_constraint=None, bias_constraint=None)(s1_gen)
@@ -58,11 +95,11 @@ s1_gen = keras.layers.convolutional.Conv2DTranspose(3, (4, 4), strides=(2, 2), p
 gen1_output = keras.layers.convolutional.Conv2D(3, 3, strides=(1, 1), padding='valid', data_format='channels_last', dilation_rate=(1, 1), activation=None, use_bias=True, kernel_initializer='glorot_uniform', bias_initializer='zeros', kernel_regularizer=None, bias_regularizer=None, activity_regularizer=None, kernel_constraint=None, bias_constraint=None)(s1_gen)
 
 s1_generator = Model(inputs=[gen1_input], outputs=[gen1_output])
-#s1_gen.summary()
+s1_generator.summary()
 
 ################################## S 1 Partial - Discriminator // S 2 Generator ###################################
 
-s1_di = keras.layers.convolutional.Conv2D(64, 3, strides=(1, 1), padding='same', data_format='channels_last', dilation_rate=(1, 1), activation='relu', use_bias=True, kernel_initializer='glorot_uniform', bias_initializer='zeros', kernel_regularizer=None, bias_regularizer=None, activity_regularizer=None, kernel_constraint=None, bias_constraint=None, input_shape=(128, 128, 6))(di1_input)
+s1_di = keras.layers.convolutional.Conv2D(64, 3, strides=(1, 1), padding='same', data_format='channels_last', dilation_rate=(1, 1), activation='relu', use_bias=True, kernel_initializer='glorot_uniform', bias_initializer='zeros', kernel_regularizer=None, bias_regularizer=None, activity_regularizer=None, kernel_constraint=None, bias_constraint=None, input_shape=(128, 128, 3))(di1_input)
 s1_di = keras.layers.convolutional.Conv2D(64, 3, strides=(1, 1), padding='same', data_format='channels_last', dilation_rate=(1, 1), activation='relu', use_bias=True, kernel_initializer='glorot_uniform', bias_initializer='zeros', kernel_regularizer=None, bias_regularizer=None, activity_regularizer=None, kernel_constraint=None, bias_constraint=None)(s1_di)
 s1_di = keras.layers.pooling.AveragePooling2D(pool_size=(2, 2), strides=(2, 2), padding='valid', data_format='channels_last')(s1_di)
 
@@ -86,29 +123,66 @@ s1_c = merge([di1_output, concatenated_vector], mode='concat',)
 s1_c = keras.layers.core.Reshape((1, 18432))(s1_c)
 s1_c = keras.layers.core.Dense(1, activation='relu', use_bias=True, kernel_initializer='glorot_uniform', bias_initializer='zeros', kernel_regularizer=None, bias_regularizer=None, activity_regularizer=None, kernel_constraint=None, bias_constraint=None)(s1_c)
 
-s1_discriminator = Model(inputs=[di1_input,concatenated_vector], outputs=[s1_c])
+s1_discriminator = Model(inputs=[di1_input, concatenated_vector], outputs=[s1_c])
 #s1_discriminator.summary()
 
 #############################################  S 1 Full model  #########################################
 
-#s1 = s1_generator
 s1 = s1_discriminator([s1_generator(gen1_input), concatenated_vector])
 train_model_s1 = Model(inputs=[gen1_input, concatenated_vector], outputs=[s1])
-train_model_s1.summary()
+
+
+#train_model_s1.summary()
 
 #from keras.utils import plot_model
-#plot_model(train_model_s1, to_file='model.png')
+#plot_model(train_model_s1, to_file='test.png')
 #plot_model(model_1, to_file='model1.png')
 #plot_model(model_2, to_file='model2.png')
 
 ##########################  Train Loop stage 1 #############################
+
+d_optim = SGD(lr=0.0005, momentum=0.9, nesterov=True)
+g_optim = SGD(lr=0.0005, momentum=0.9, nesterov=True)
+s1_generator.compile(loss='binary_crossentropy', optimizer="SGD")
+train_model_s1.compile(
+loss='binary_crossentropy', optimizer=g_optim)
+s1_discriminator.trainable = True
+s1_discriminator.compile(loss='binary_crossentropy', optimizer=d_optim)
+
+for n in range(294, 304):
+    img_path = img_names[n] #Real Img
+    try:
+        img = image.load_img(os.path.join("./pubfig_resized/",img_path), target_size=(128, 128))
+    except Exception as e:
+        print(e)
+        print(os.path.join("./pubfig_resized/",img_path))
+        continue
+    img = image.img_to_array(img)
+    print("image" + str(img.shape))
+    print(encoding[n])
+    conc = np.concatenate([encoding[n], np.zeros(54)])
+    print(conc.shape)
+    enc = np.tile(conc,16)
+    print(enc.shape)
+    enc = enc.reshape((1,4,4,128)) #Textual embedding
+    noise = np.random.normal(size=(1,4,4,1024)) # Noise vector
+    generator_input = np.concatenate((noise, enc), axis=3)
+
+    print(generator_input.shape, type(generator_input), gen1_input.shape, type(gen1_input))
+    synth_img = s1_generator.predict(x=generator_input) #Generated synthetic image
+    both_img = np.array([img,synth_img]) #Both images in array
+    both_gt = [1,0] #Ground truth values for both images
+    s1_discriminator.train_on_batch(x=[both_img, enc], y=both_gt)
+    train_model_s1.train_on_batch(x=np.array([img]), y=np.array([1]))
+
+
 
 
 
 
 ################################################## S 2 Generator  ##########################################################
 
-s2_gen = keras.layers.convolutional.Conv2D(64, 3, strides=(1, 1), padding='same', data_format='channels_last', dilation_rate=(1, 1), activation='relu', use_bias=True, kernel_initializer='glorot_uniform', bias_initializer='zeros', kernel_regularizer=None, bias_regularizer=None, activity_regularizer=None, kernel_constraint=None, bias_constraint=None, input_shape=(128, 128, 3))(gen2_input)
+'''s2_gen = keras.layers.convolutional.Conv2D(64, 3, strides=(1, 1), padding='same', data_format='channels_last', dilation_rate=(1, 1), activation='relu', use_bias=True, kernel_initializer='glorot_uniform', bias_initializer='zeros', kernel_regularizer=None, bias_regularizer=None, activity_regularizer=None, kernel_constraint=None, bias_constraint=None, input_shape=(128, 128, 3))(gen2_input)
 s2_gen = keras.layers.convolutional.Conv2D(64, 3, strides=(1, 1), padding='same', data_format='channels_last', dilation_rate=(1, 1), activation='relu', use_bias=True, kernel_initializer='glorot_uniform', bias_initializer='zeros', kernel_regularizer=None, bias_regularizer=None, activity_regularizer=None, kernel_constraint=None, bias_constraint=None)(s2_gen)
 s2_gen = keras.layers.pooling.AveragePooling2D(pool_size=(2, 2), strides=(2, 2), padding='valid', data_format='channels_last')(s2_gen)
 
@@ -225,3 +299,5 @@ s2_discriminator = Model(inputs=[di2_input,concatenated_vector], outputs=[s2_c2]
 
 
 ####################################  TrainLoop Stage 2  ####################################
+
+'''
